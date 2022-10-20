5ea322d90c6bc6d86af8aa52652e6e0af5413748
shalu devda <94682838+shalu911@users.noreply.github.com>
2022-10-20 23:44:07 +0530

    Add files via upload

---
+++ heart-trail-annimation/script.js
@@ -0,0 +1,20 @@
+const bodyE1 = document.querySelector("body");
+
+bodyE1.addEventListener("mousemove", (Event)=>
+{
+    const xpos = Event.offsetX;
+    const ypos = Event.offsetY;
+    const spanE1 = document.createElement("span");
+    spanE1.style.left = xpos +"px";
+    spanE1.style.top = ypos + "px";
+    const size = Math.random()*100;
+    spanE1.style.width=size+"px";
+    spanE1.style.height=size+"px";
+bodyE1.appendChild(spanE1);
+setTimeout(()=>{
+spanE1.remove();
+
+},3000);
+
+
+});
